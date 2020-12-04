d19c2a828e484d944c59f8381b851502ed7e75b5
Theo Hennessy <theo.hennessy@gmail.com>
2020-12-04 11:49:07 +0100

    Two stacks for input and output; added Docstrings to SteinCliIo

--- cli_io.py
+++ cli_io.py
@@ -1,7 +1,19 @@
 class SteinCliIo(object):
+  """
+  Manages input and output (IO) in the command line interface (CLI).
+  """
   def __init__(self):
-    self.stack = []
+    # the history of received inputs
+    self.input_stack = []
+    # the history of printed outputs
+    self.output_stack = []
 
   def print(self, msg):
-    self.stack.append(msg)
+    """
+    Prints a message to the CLI and appends it to the output stack.
+
+    Arguments:
+      - msg (string): message to be printed.
+    """
     print(msg)
+    self.output_stack.append(msg)
