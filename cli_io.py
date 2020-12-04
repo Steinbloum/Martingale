abc063dd1a0633c4adc0ff503ddced4e6c36a412
Theo Hennessy <theo.hennessy@gmail.com>
2020-12-04 12:10:27 +0100

    Added a print_separator method; added to_stack parameter to print method

--- cli_io.py
+++ cli_io.py
@@ -8,20 +8,41 @@ class SteinCliIo(object):
     # the history of printed outputs
     self.output_stack = []
 
-  def print(self, msg):
+  def print(self, msg, to_stack=True):
     """
     Prints a message to the CLI and appends it to the output stack.
 
-    Arguments:
+    Parameters:
       - msg (string): message to be printed.
+      - to_stack (boolean): whether to append to output_stack
+                            defaults to True
     """
     print(msg)
-    self.output_stack.append(msg)
+    if to_stack:
+      self.output_stack.append(msg)
 
   def display_welcome(self):
+    """
+    Displays the welcome text.
+    """
     intro = "Hello gambler, you are playing roulette Martingale.\n\n" \
       "Rules are simple, you change colors when you win, you double down if you loose.\n" \
       "Get rich or get rekt trying, can you beat the odds ?\n\n" \
-      "Choose an objective, game will continue until objective is met or until you have nothing left.\n" \
-      "******************************************"
+      "Choose an objective, game will continue until objective is met or until you have nothing left.\n"
     self.print(intro)
+    self.print_separator()
+
+  def print_separator(self, character="*", length=32, padding=1):
+    """
+    Prints a visual separator in the CLI.
+
+    Parameters:
+      - character (string): the symbol to use as a separator
+      - length (integer): the length of the separator
+      - padding (integer): number of blank lines above/below separator
+    """
+    for i in range(padding):
+      self.print("", to_stack=True)
+    self.print(character * length, to_stack=True)
+    for i in range(padding):
+      self.print("", to_stack=True)
